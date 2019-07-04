fb8d7f92e70509a4230739fbc4466d5b67eb0e06
wangjianhua <1270063771@qq.com>
2019-07-04 16:14:10 +0800

    HelloWorld路由

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Django基础学习
 
 ##### 1. django-admin startproject [name]
+##### 2. python manage.py runserver 0.0.0.0:8000  #启动项目，自动检测文件变化动态加载
+##### 3. 新建view文件和hello()方法，配置urls下路由
+
 
--- django_demo/urls.py
+++ django_demo/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path  # Django>=2.0 re_path用于路由规则解析
+from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', view.hello)
 ]
